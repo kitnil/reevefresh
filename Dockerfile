35f90911c9ff52bcf4e49043a5daf398d3100d06
Oleg Pykhalov <go.wigust@gmail.com>
2020-05-16 18:44:17 +0300

    reevefresh: Authenticate to Twitch with a bearer.
    * reevefresh/__main__.py (main): Authenticate to Twitch with a bearer.
    * Dockerfile (label)[my.docker.cmd]: Pass new environment variables.

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,6 @@ RUN pip install twitch-python \
 COPY reevefresh/__main__.py /bin/reevefresh
 RUN chmod a+x /bin/reevefresh
 
-LABEL "my.docker.cmd"="docker run --restart=unless-stopped --name reevefresh --detach --network=host --env SLACK_API_KEY=$SLACK_API_KEY --env TWITCH_API_KEY=$TWITCH_API_KEY localhost:5000/reevefresh:latest -u arhont_tv,blackufa,squirrel"
+LABEL "my.docker.cmd"="docker run --restart=unless-stopped --name reevefresh --detach --network=host --env SLACK_API_KEY=$SLACK_API_KEY --env TWITCH_CLIENT_ID=$TWITCH_CLIENT_ID --env TWITCH_CLIENT_SECRET=$TWITCH_CLIENT_SECRET --env TWITCH_REDIRECT_URI=$TWITCH_REDIRECT_URI --env TWITCH_RESPONCE_TYPE=$TWITCH_RESPONCE_TYPE --env TWITCH_SCOPE=$TWITCH_SCOPE localhost:5000/reevefresh:latest -u arhont_tv,blackufa,squirrel"
 
 ENTRYPOINT ["/bin/reevefresh"]
--- reevefresh/__main__.py
+++ reevefresh/__main__.py
@@ -3,6 +3,8 @@
 import argparse
 import time
 import os
+import http.client
+import json
 from twitch import Helix
 from slack import WebClient
 
@@ -12,9 +14,26 @@ def main():
     parser.add_argument("-i", "--interval", default=30, help="Update interval in seconds", type=int)
     parser.add_argument("-u", "--users", default="", help="Twitch users")
     args = parser.parse_args()
+
     remember_online_users = []
+
+    TWITCH_CLIENT_ID=os.environ["TWITCH_CLIENT_ID"]
+    TWITCH_CLIENT_SECRET=os.environ["TWITCH_CLIENT_SECRET"]
+    TWITCH_REDIRECT_URI=os.environ["TWITCH_REDIRECT_URI"]
+    TWITCH_RESPONCE_TYPE=os.environ["TWITCH_RESPONCE_TYPE"]
+    TWITCH_SCOPE=os.environ["TWITCH_SCOPE"]
+    def bearer():
+        connection = http.client.HTTPSConnection("id.twitch.tv")
+        connection.request("POST", "/oauth2/token?client_id=" + TWITCH_CLIENT_ID
+                           + "&client_secret=" + TWITCH_CLIENT_SECRET
+                           + "&grant_type=client_credentials")
+        response = connection.getresponse()
+        return json.loads(response.read().decode('utf8'))['access_token']
+
     while(True):
-        twitch = Helix(os.environ["TWITCH_API_KEY"])
+        twitch = Helix(client_id=TWITCH_CLIENT_ID,
+                       client_secret=TWITCH_CLIENT_SECRET,
+                       bearer_token=bearer())
         slack = WebClient(token=os.environ["SLACK_API_KEY"])
         for user in args.users.split(","):
             if twitch.user(user).is_live:
