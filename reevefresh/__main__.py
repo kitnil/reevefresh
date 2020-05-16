2e165da6cca003e690a4d74ddb6b6f8a0dd577a3
Oleg Pykhalov <go.wigust@gmail.com>
2020-05-16 21:41:05 +0300

    reevefresh: Handle HTTPError exception.

--- reevefresh/__main__.py
+++ reevefresh/__main__.py
@@ -11,6 +11,8 @@ from slack import WebClient
 def main():
     parser = argparse.ArgumentParser(description="Twitch notifications")
     parser.add_argument("-c", "--channel", default="#video", help="Slack channel")
+    parser.add_argument("-n", "--notifications", default="#ci",
+                        help="Slack channel notifications for errors or warnings")
     parser.add_argument("-i", "--interval", default=30, help="Update interval in seconds", type=int)
     parser.add_argument("-u", "--users", default="", help="Twitch users")
     args = parser.parse_args()
@@ -35,15 +37,23 @@ def main():
                        client_secret=TWITCH_CLIENT_SECRET,
                        bearer_token=bearer())
         slack = WebClient(token=os.environ["SLACK_API_KEY"])
-        for user in args.users.split(","):
-            if twitch.user(user).is_live:
-                print("TWITCH_LIVE: {}".format(user))
-                if user not in remember_online_users:
-                    remember_online_users.append(user)
-                    slack.chat_postMessage(channel=args.channel, text="TWITCH_LIVE: https://www.twitch.tv/{}".format(user))
-            else:
-                if user in remember_online_users:
-                    remember_online_users.remove(user)
+        try:
+            for user in args.users.split(","):
+                if twitch.user(user).is_live:
+                    print("TWITCH_LIVE: {}".format(user))
+                    if user not in remember_online_users:
+                        remember_online_users.append(user)
+                        slack.chat_postMessage(channel=args.channel,
+                                               attachments=[{"text": "TWITCH_LIVE: https://www.twitch.tv/{}".format(user),
+                                                             "color": "#ff0000"}])
+                else:
+                    if user in remember_online_users:
+                        remember_online_users.remove(user)
+        except error.HTTPError as exception:
+            print("ERROR: {}".format(exception))
+            slack.chat_postMessage(channel=args.notifications,
+                                   attachments=[{"text": "ERROR: Failed to receive Twitch user status: {}".format(exception),
+                                                 "color": "#ff0000"}])
         print("Online users: {}\n".format(",".join(remember_online_users)))
         time.sleep(args.interval)
 
